2a0f65d525de937acdd2d3174b1307bbe4cf4572
Dan Taylor <danieljtaylor@gmail.com>
2018-06-22 21:34:14 -0600

    only jump up one directory from ./build.sh

--- api/build.sh
+++ api/build.sh
@@ -9,4 +9,4 @@ fi
 
 dotnet restore
 #dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/deploy-package.zip
-dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package ../../artifacts/deploy-package.zip
+dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package ../artifacts/deploy-package.zip
