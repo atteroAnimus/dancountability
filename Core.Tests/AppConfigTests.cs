7f576ee5dd580678e60b70a6a730e5f2a99cfe65
Dan Taylor <danieljtaylor@gmail.com>
2018-06-29 10:02:29 -0600

    Auto stash before merge of "develop" and "origin/develop"

---
+++ Core.Tests/AppConfigTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Common;
+using Data;
+using IocFactory;
+using Xunit;
+using Xunit.Abstractions;
+namespace Core.Tests
+{
+	
+	public class AppConfigTests
+	{
+		[Fact]
+		public void TestInstantiate()
+		{
+			var thing = Factory.Instance.Resolve<IAppConfig>();
+			Assert.True(thing is AppConfig);
+			Assert.NotNull(Factory.Instance.Resolve<IAppConfig>());
+			Assert.NotNull(thing);
+			Assert.NotNull(Factory.Instance.Resolve<IData>());
+			
+		}
+	}
+}
