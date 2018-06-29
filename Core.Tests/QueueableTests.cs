912480b6f48864dade428519f6739ecfaf9c544d
Dan Taylor <danieljtaylor@gmail.com>
2018-06-29 14:10:41 -0600

    branch filters and queue function

---
+++ Core.Tests/QueueableTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using IocFactory;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Core.Tests
+{
+	public class QueueableTests
+	{
+		[Fact]
+		public void TestResolveQueueable()
+		{
+			var queueable = Factory.Instance.Resolve<IQueuable>();
+			Assert.NotNull(queueable);
+		}
+	}
+}
